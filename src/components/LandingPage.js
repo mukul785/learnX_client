f4a1653a74ebc69e59cc1aee1ac14078e37a5423
mukul785 <156766802+mukul785@users.noreply.github.com>
2025-05-23 18:13:10 +0530

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -49,8 +49,7 @@ const LandingPage = () => {
             </div>
             <div className="right-column">
                 <div className="image-placeholder">
-                    {/* Placeholder for image */}
-                    <img src="/path-to-your-image.png" alt="Learning illustration" />
+                    <img src="https://images.unsplash.com/photo-1616017640739-44ce2bfd9b4e?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Learning illustration" />
                 </div>
             </div>
         </div>
